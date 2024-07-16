1b9c92db8015dba4ca5bcd904c86647d8f20152b
unknown <phamlong123np@gmail.com>
2024-07-16 16:57:16 +0700

    Migration

---
+++ database/migrations/2024_07_16_072559_add_provider_to_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            if(!Schema::hasColumn('users','provider')){
+                $table->string('provider',50)->nullable()->after('password');
+            }
+            if(!Schema::hasColumn('users','provider_id')){
+                $table->double('provider_id')->nullable()->after('password');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['provider_id','provider']);
+        });
+    }
+};
